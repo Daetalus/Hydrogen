653cf1c95a2875fcdb103ee39694fc3a1ddafa77
Ben Anderson <gravityscore@gmail.com>
2016-01-20 18:12:26 +0800

    Add instruction manipulation and parser struct definition

--- src/vm/ins.h
+++ src/vm/ins.h
@@ -1,6 +1,6 @@
 
 //
-//  Bytecode Instructions
+//  Instructions
 //
 
 #ifndef INS_H
@@ -9,5 +9,40 @@
 #include <stdint.h>
 
 #include "bytecode.h"
+
+
+// A bytecode instruction (64 bits).
+typedef uint64_t Instruction;
+
+
+// Create a new instruction from an opcode and 3 arguments.
+static inline Instruction ins_new(BytecodeOpcode opcode, uint16_t arg1,
+		uint16_t arg2, uint16_t arg3) {
+	return ((Instruction) opcode) |
+		(((Instruction) arg1) << 16) |
+		(((Instruction) arg2) << 32) |
+		(((Instruction) arg3) << 48);
+}
+
+
+// Returns the `n`th argument of an instruction. Argument 0 is the instruction's
+// opcode.
+static inline uint16_t ins_arg(Instruction instruction, uint32_t n) {
+	uint32_t offset = n * 16;
+	uint64_t selection = ((uint64_t) 0xffff) << offset;
+	return (uint16_t) ((instruction & selection) >> offset);
+}
+
+
+// Sets the `n`th argument of an instruction. Argument 0 is the instruction's
+// opcode.
+static inline Instruction ins_set(Instruction instruction, uint32_t n,
+		uint16_t value) {
+	uint32_t offset = n * 16;
+	uint64_t selection = (((uint64_t) 0xffff) << offset);
+	uint64_t to_clear = (((uint64_t) 0xffffffffffffffff) ^ selection);
+	uint64_t cleared = instruction & to_clear;
+	return cleared | (((uint64_t) value) << offset);
+}
 
 #endif
---
+++ src/vm/parser.h
@@ -0,0 +1,79 @@
+
+//
+//  Parser
+//
+
+#ifndef PARSER_H
+#define PARSER_H
+
+#include <hydrogen.h>
+#include <stdbool.h>
+
+#include "vec.h"
+
+
+// Data associated with a loop so we know where to point break statement's jump
+// instructions once the whole loop has been compiled.
+typedef struct loop {
+	// The outer loop this one is contained inside of, or NULL if there are no
+	// enclosing loops.
+	struct loop *parent;
+
+	// The head of the jump list for all break statements inside this loop.
+	Index head;
+} Loop;
+
+
+// Each function is parsed in their own scope.
+typedef struct function_scope {
+	// The parent function scope in which this function is defined (or NULL if
+	// this function scope is the top level of a package).
+	struct function_scope *parent;
+
+	// The index of the function being parsed in the interpreter's function
+	// list. Bytecode instructions are emitted into this function.
+	Index fn_index;
+
+	// Set to true when this function scope is a method on a struct.
+	bool is_method;
+
+	// The base local in the parser's locals stack which begins the locals
+	// defined inside this function. The instance of the struct (`self`) is
+	// first (present only if this is a method), then the arguments to the
+	// function, then the locals defined in the function.
+	uint32_t first_local;
+
+	// A linked list of loops so we know which loop to break out of when we
+	// encounter a break statement. The inner most loop is stored at the head
+	// of the linked list (this pointer).
+	Loop *loop;
+} FunctionScope;
+
+
+// Parses source code into bytecode instructions.
+typedef struct {
+	// A pointer to the interpreter state that functions, packages, etc will be
+	// created on.
+	HyState *state;
+
+	// The lexer, emitting tokens from source code that the parser transforms
+	// into more cohesive language structures.
+	Lexer lexer;
+
+	// All permanent locals defined on the stack by the source code we're
+	// parsing. The length of this vector is the number of active locals (ie.
+	// locals that have been given a name). Expressions, function calls, etc
+	// all use temporary locals on top of these.
+	Vec(Local) locals;
+
+	// The actual number of locals (including temporary ones).
+	uint32_t locals_count;
+
+	// Each function is parsed in its own scope. Functions defined inside other
+	// functions have their scopes linked together by a linked list. The head
+	// of the linked list (this pointer) is the inner most function (the one
+	// currently being parsed).
+	FunctionScope *scope;
+} Parser;
+
+#endif
