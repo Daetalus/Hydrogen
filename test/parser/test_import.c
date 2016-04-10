d45963b186026ee4036bbd975ec715a8d16b3106
Ben Anderson <gravityscore@gmail.com>
2016-04-10 14:31:33 +0800

    Convert import tests

--- test/parser/test_import.c
+++ test/parser/test_import.c
@@ -3,37 +3,32 @@
 //  Import Tests
 //
 
-extern "C" {
+#include <test.h>
 #include <hydrogen.h>
 #include <import.h>
-#include <vec.h>
-#include <pkg.h>
-}
-
-#include <gtest/gtest.h>
 
 
 // Tests validating import paths
-TEST(ImportPath, Validation) {
-	ASSERT_EQ(import_is_valid("hello"), true);
-	ASSERT_EQ(import_is_valid("hello/test"), true);
-	ASSERT_EQ(import_is_valid("this/is/a/test"), true);
-	ASSERT_EQ(import_is_valid("/absolute"), true);
-	ASSERT_EQ(import_is_valid("/an/absolute/path"), true);
-	ASSERT_EQ(import_is_valid("../relative"), true);
-	ASSERT_EQ(import_is_valid("../relative/path/with/components"), true);
-	ASSERT_EQ(import_is_valid("some/more/../relative"), true);
-	ASSERT_EQ(import_is_valid("some/../more/../../relative"), true);
+void test_validation(void) {
+	eq_int(import_is_valid("hello"), true);
+	eq_int(import_is_valid("hello/test"), true);
+	eq_int(import_is_valid("this/is/a/test"), true);
+	eq_int(import_is_valid("/absolute"), true);
+	eq_int(import_is_valid("/an/absolute/path"), true);
+	eq_int(import_is_valid("../relative"), true);
+	eq_int(import_is_valid("../relative/path/with/components"), true);
+	eq_int(import_is_valid("some/more/../relative"), true);
+	eq_int(import_is_valid("some/../more/../../relative"), true);
 
-	ASSERT_EQ(import_is_valid(""), false);
-	ASSERT_EQ(import_is_valid("/this/is/a/test/"), false);
-	ASSERT_EQ(import_is_valid("/thi.s/is/a/tes.t"), false);
-	ASSERT_EQ(import_is_valid("/this/is/testin/./"), false);
-	ASSERT_EQ(import_is_valid("/this/../.is/testin"), false);
-	ASSERT_EQ(import_is_valid("./more"), false);
-	ASSERT_EQ(import_is_valid("empty//path/component"), false);
-	ASSERT_EQ(import_is_valid("empty////path//components"), false);
-	ASSERT_EQ(import_is_valid("invalid/@#FJ($!@#F\n\t/characters"), false);
+	eq_int(import_is_valid(""), false);
+	eq_int(import_is_valid("/this/is/a/test/"), false);
+	eq_int(import_is_valid("/thi.s/is/a/tes.t"), false);
+	eq_int(import_is_valid("/this/is/testin/./"), false);
+	eq_int(import_is_valid("/this/../.is/testin"), false);
+	eq_int(import_is_valid("./more"), false);
+	eq_int(import_is_valid("empty//path/component"), false);
+	eq_int(import_is_valid("empty////path//components"), false);
+	eq_int(import_is_valid("invalid/@#FJ($!@#F\n\t/characters"), false);
 }
 
 
@@ -42,7 +37,7 @@ TEST(ImportPath, Validation) {
 	char *package_heap = (char *) malloc(strlen(package) + 1); \
 	strcpy(package_heap, package);                             \
 	char *result = import_pkg_path(parent, package_heap);      \
-	ASSERT_STREQ(result, expected);                            \
+	eq_str(result, expected);                                  \
 	free(result);                                              \
 	if (result != package_heap) {                              \
 		free(package_heap);                                    \
@@ -52,7 +47,7 @@ TEST(ImportPath, Validation) {
 
 // Tests resolving import paths to their actual locations on the filesystem
 // using the parent package
-TEST(ImportPath, PathResolution) {
+void test_path_resolution(void) {
 	ASSERT_PACKAGE_PATH(NULL, "hello", "hello");
 	ASSERT_PACKAGE_PATH(NULL, "test/ing", "test/ing");
 	ASSERT_PACKAGE_PATH(NULL, "/abs/path", "/abs/path");
@@ -74,13 +69,13 @@ TEST(ImportPath, PathResolution) {
 // Asserts a package name matches its expected value
 #define ASSERT_PACKAGE_NAME(path, expected) { \
 	char *name = hy_pkg_name(path);           \
-	ASSERT_STREQ(name, expected);             \
+	eq_str(name, expected);                   \
 	free(name);                               \
 }
 
 
 // Tests extracting the name of a package from its path
-TEST(ImportPath, PackageName) {
+void test_package_name(void) {
 	ASSERT_PACKAGE_NAME("test", "test");
 	ASSERT_PACKAGE_NAME("a", "a");
 	ASSERT_PACKAGE_NAME("test/testing", "testing");
@@ -92,4 +87,12 @@ TEST(ImportPath, PackageName) {
 	ASSERT_PACKAGE_NAME("../test.hy", "test");
 	ASSERT_PACKAGE_NAME("another/../test.hy", "test");
 	ASSERT_PACKAGE_NAME("../another/../test.hy", "test");
+}
+
+
+int main(int argc, char *argv[]) {
+	test_pass("Path validation", test_validation);
+	test_pass("Path resolution", test_path_resolution);
+	test_pass("Extract package name", test_package_name);
+	return test_run(argc, argv);
 }
